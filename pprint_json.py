de849f4ee33c0ea99798c4a7f2619fbf32fa0edb
WxMikM <den463@yandex.ru>
2017-03-01 22:01:32 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,25 @@
 import json
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file_handler:
+            return json.load(file_handler)
+    except OSError:
+        print('файл "{0}" не может быть открыт'.format(file_path))
+    return None
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(list_json):
+    print(json.dumps(list_json, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        json_path = input('Введите путь к файлу .json : ')
+
+    list_records = load_data(sys.argv[1] if len(sys.argv) > 1 else json_path)
+
+    if list_records is not None:
+        pretty_print_json(list_records)
